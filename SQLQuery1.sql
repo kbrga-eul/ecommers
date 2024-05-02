0349ce88da190782fb5435666eb029aee967e742
Kübra <91066410+kbrga-eul@users.noreply.github.com>
2024-05-02 11:36:40 +0300

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,6 @@
+﻿select A.*,dbo.getSizeName(2) as SizeNamee,'2' as SizeIDD,SizeData.Name,SizeData.Extention
+from tblProducts A
+cross apply(
+select top 1 B.Name,Extention from tblProductImages B where B.PID=A.PID
+) SizeData
+where A.PID=10005
